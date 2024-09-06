eb181eff06fb72051cff39efe72b43f5ecc6a18d
surajgjadhav <surajjadhavasmi@gmail.com>
2024-09-06 20:49:30 +0530

    Added extension origin to CORS
    Signed-off-by: surajgjadhav <surajjadhavasmi@gmail.com>

--- api/config/corsOptions.js
+++ api/config/corsOptions.js
@@ -1,10 +1,19 @@
-const allowdedOrigins = ["http://127.0.0.1:5173", "http://localhost:5173"];
+const allowdedOrigins = [
+  "http://127.0.0.1:5173",
+  "http://localhost:5173",
+  "chrome-extension://*", // replace star with your extension ID
+];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowdedOrigins.includes(origin)) {
+    if (
+      allowdedOrigins.includes(origin) ||
+      origin.startsWith("chrome-extension://")
+    ) {
       callback(null, true);
     } else {
+      console.log("origin: ", origin);
+
       callback(new Error("CORS Error: Not Allowded"));
     }
   },
